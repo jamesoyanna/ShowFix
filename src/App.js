255dd5d60c1f90f8b668003a65612be4e22843f4
jamesoyanna <engrjayt200@gmail.com>
2023-07-18 12:25:08 +0100

    Implement Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import MoviePage from './components/MoviePage/MoviePage';
 
 function App() {
   return (
-    <div className="App">
-     <h3>ShowFix Movies</h3>
-    </div>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/movie-page' element={<MoviePage />} />
+   </Routes>
   );
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './home.css'
+const Home = () => {
+  return (
+    <div className='home'>ShowFix Movies</div>
+  )
+}
+
+export default Home
