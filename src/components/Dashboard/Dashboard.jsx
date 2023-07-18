2ef2c8c02e3b5877530a59ea41436b8203f0cfcc
jamesoyanna <engrjayt200@gmail.com>
2023-07-18 15:06:05 +0100

    Implement searchbar component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { FiSearch } from 'react-icons/fi';
 import MovieCard from '../MovieCard/MovieCard';
+import './dashboard.css';
 
 const Dashboard = () => {
   const movies = useSelector((state) => state.movies);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    // Implement search functionality here
+    // Filter movies based on the search query
+  };
 
   return (
     <div className="dashboard">
-      <h1 className="heading">ShowFlix</h1>
+      <h1 className="heading">Explore</h1>
+      <div className="search-bar">
+        <div className="search-input">
+          <FiSearch className="search-icon" />
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search"
+          />
+        </div>
+        <button className="search-button" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
       <div className="movie-list">
         {movies.loading ? (
           <p>Loading...</p>
