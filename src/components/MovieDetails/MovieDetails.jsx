135b4975e7908b0e2387f90deef9b4dc7e65f8a2
jamesoyanna <engrjayt200@gmail.com>
2023-07-19 17:56:16 +0100

    Fix styling issue on Movie details

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './moviedetails.css';
+import { FaArrowLeft } from 'react-icons/fa';
 
-const MovieDetails = ({ selectedMovie ,onClose}) => {
-
+const MovieDetails = ({ selectedMovie, onClose }) => {
   return (
     <div className="movie-details">
-     
-    {selectedMovie && (
-      <div className="movie-card">
-        <div className="image-container">
-          <img src={selectedMovie.image} alt={selectedMovie.title} className="cover-image" />
+        <div>
+            <FaArrowLeft onClick={onClose} />
+          </div>
+        <div className="movie-card">
+        
+          <div className="image-container">
+            <img src={selectedMovie.image} alt={selectedMovie.title} className="cover-image" />
+          </div>
+          <h3>{selectedMovie.title}</h3>
+          <p>{selectedMovie.rated}</p>
+          <p>{selectedMovie.released}</p>
+          <p>{selectedMovie.genre}</p>
+          <p>{selectedMovie.director}</p>
+          <h3>{selectedMovie.plot}</h3>
+          <p>{selectedMovie.language}</p>
+          <p>{selectedMovie.awards}</p>
+          <button className="watch-button">Watch</button>
         </div>
-        <h3>{selectedMovie.title}</h3>
-        <p>{selectedMovie.rated}</p>
-        <p>{selectedMovie.released}</p>
-        <p>{selectedMovie.genre}</p>
-        <p>{selectedMovie.director}</p>
-        <h3>{selectedMovie.plot}</h3>
-        <p>{selectedMovie.language}</p>
-        <p>{selectedMovie.awards}</p>
-        <button className="watch-button">Watch</button>
-      </div>
-    )}
-  </div>
+    </div>
   );
 };
 
 export default MovieDetails;
+
