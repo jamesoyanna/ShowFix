949978f1bab1a450955e02a282b8dd34e81701a3
jamesoyanna <engrjayt200@gmail.com>
2023-07-20 21:18:19 +0100

    Fix display of searchbar icon issue on mobile

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './searchbar.css';
 
 const SearchBar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState('');
-
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
   const handleSearch = () => {
     onSearch(searchQuery);
   };
@@ -14,7 +14,46 @@ const SearchBar = ({ onSearch }) => {
     onSearch(event.target.value);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
+    <>
+    {isMobile ? (
+                <div className="search-bar">
+                <div className="search-input">
+                  <input type="text"   value={searchQuery} onChange={handleChange} placeholder="Search" />
+                </div>
+                <FiSearch className="searchbar-icon" onClick={handleSearch} />
+              </div>
+      ) : (
+        <div className="search-bar">
+        <div className="search-input">
+         <FiSearch className="search-icon" />
+         <input
+          type="text"
+          value={searchQuery}
+          onChange={handleChange}
+          placeholder="Search"
+          />
+        </div>
+        <button className="search-button" onClick={handleSearch}>
+          Search
+          </button>
+        </div>
+      )}
+    
+    
+{/*    
     <div className="search-bar">
       <div className="search-input">
         <FiSearch className="search-icon" />
@@ -28,7 +67,8 @@ const SearchBar = ({ onSearch }) => {
       <button className="search-button" onClick={handleSearch}>
         Search
       </button>
-    </div>
+    </div> */}
+    </>
   );
 };
 
