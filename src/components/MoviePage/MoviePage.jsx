743a5fa07566cce2584adbfc6c06cf388ccdd7cc
jamesoyanna <engrjayt200@gmail.com>
2023-07-21 10:15:45 +0100

    Update styling on MoviePage

--- src/components/MoviePage/MoviePage.jsx
+++ src/components/MoviePage/MoviePage.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { FaCalendarAlt, FaStar, FaClock } from 'react-icons/fa';
 import './moviepage.css';
 import Sidebar from '../Sidebar/Sidebar';
-
+import PlaceholderImage from '../../images/mov1.png'
 const MoviePage = () => {
   const { id } = useParams();
   const movies = useSelector((state) => state.movies);
   const selectedMovie = movies.data.find((movie) => movie.id === id);
 
-  const imageSource = selectedMovie.image || 'path/to/placeholder-image.jpg'; // Use a placeholder image if no image is available
+  
+  const imageSource = selectedMovie.image || PlaceholderImage;
 
   return (
     <div className="movie-page-container">
-      <Sidebar />
-      <div className="movie-details">
-        <div className="details-row">
-          <div className="image-container">
-            <img src={imageSource} alt={selectedMovie.title} className="cover-image" />
+    <Sidebar />
+    <div className="movie-details">
+      <div className="details-row">
+        <div className="movie-image">
+          <img src={imageSource} alt={selectedMovie.title} />
+        </div>
+        <div className="info-container">
+          <h3>{selectedMovie.title}</h3>
+          <p>{selectedMovie.plot}</p>
+          <div className="rating-time-row">
+          <p> <FaCalendarAlt />{selectedMovie.released}</p>
+            <p className="rating"> <FaStar />{selectedMovie.rated}</p>
+            <p> <FaClock />{selectedMovie.runtime}</p>
           </div>
-          <div className="info-container">
-            <h3>{selectedMovie.title}</h3>
-            <p>{selectedMovie.plot}</p>
-            <div className="rating-time-row">
-              <p className="rating">{selectedMovie.rated}</p>
-              <p>{selectedMovie.released}</p>
-              <p>{selectedMovie.runtime}</p>
-            </div>
+          <div className="watch-button-container">
             <button className="watch-button">Watch Now</button>
             <div className="heart-icon-container">
               <div className="heart-icon">&#x2665;</div>
@@ -35,6 +38,7 @@ const MoviePage = () => {
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
