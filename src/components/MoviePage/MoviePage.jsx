19815ab93572f0f65d8af5d9e9b714c26ad4f497
jamesoyanna <engrjayt200@gmail.com>
2023-07-21 08:48:31 +0100

    Update MoviePage

--- src/components/MoviePage/MoviePage.jsx
+++ src/components/MoviePage/MoviePage.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import './moviepage.css'
+import './moviepage.css';
+import Sidebar from '../Sidebar/Sidebar';
 
 const MoviePage = () => {
-  const { id } = useParams(); // Step 2: Get the movieId from URL parameters
+  const { id } = useParams();
   const movies = useSelector((state) => state.movies);
-  
-  console.log("Movie Id", id);
-
-  // Step 3: Find the movie with the matching id in the movies array
   const selectedMovie = movies.data.find((movie) => movie.id === id);
 
+  const imageSource = selectedMovie.image || 'path/to/placeholder-image.jpg'; // Use a placeholder image if no image is available
+
   return (
-    <div className="movie-details">
-      {/* Display the movie details */}
-  
-          <div>
-            {/* Display the movie details using selectedMovie */}
-            <img src={selectedMovie.image} alt={selectedMovie.title} className="cover-imag" />
-            <h3>{selectedMovie.title}</h3>
-          <p>{selectedMovie.rated}</p>
-          <p>{selectedMovie.released}</p>
-          <p>{selectedMovie.genre}</p>
-          <p>{selectedMovie.director}</p>
-          <h3>{selectedMovie.plot}</h3>
-          <p>{selectedMovie.language}</p>
-          <p>{selectedMovie.awards}</p>
-           
-            <button className="watch-button">Watch</button>
+    <div className="movie-page-container">
+      <Sidebar />
+      <div className="movie-details">
+        <div className="details-row">
+          <div className="image-container">
+            <img src={imageSource} alt={selectedMovie.title} className="cover-image" />
           </div>
-  
+          <div className="info-container">
+            <h3>{selectedMovie.title}</h3>
+            <p>{selectedMovie.plot}</p>
+            <div className="rating-time-row">
+              <p className="rating">{selectedMovie.rated}</p>
+              <p>{selectedMovie.released}</p>
+              <p>{selectedMovie.runtime}</p>
+            </div>
+            <button className="watch-button">Watch Now</button>
+            <div className="heart-icon-container">
+              <div className="heart-icon">&#x2665;</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
