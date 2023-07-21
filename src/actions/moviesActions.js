8b16d1f643baf66174e131078de8ac48a4e6a1cf
jamesoyanna <engrjayt200@gmail.com>
2023-07-21 13:05:40 +0100

    Update .env file

--- src/actions/moviesActions.js
+++ src/actions/moviesActions.js
@@ -1,14 +1,18 @@
+// movies.js (or whatever your filename is)
+
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+const OMDB_BASE_URL = process.env.REACT_APP_OMDB_BASE_URL;
+const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
 export const fetchMovies = createAsyncThunk('movies/fetchMovies', async (searchQuery) => {
   try {
-    const response = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=25d01e11&s=${searchQuery}`);
+    const response = await axios.get(`${OMDB_BASE_URL}?i=tt3896198&apikey=${OMDB_API_KEY}&s=${searchQuery}`);
     const { data } = response;
     if (data.Response === 'True' && Array.isArray(data.Search)) {
       const movies = await Promise.all(
         data.Search.map(async (movie) => {
-          const movieResponse = await axios.get(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=25d01e11`);
+          const movieResponse = await axios.get(`${OMDB_BASE_URL}?i=${movie.imdbID}&apikey=${OMDB_API_KEY}`);
           const movieData = movieResponse.data;
           return {
             id: movie.imdbID,
@@ -33,16 +37,12 @@ export const fetchMovies = createAsyncThunk('movies/fetchMovies', async (searchQ
   }
 });
 
-
 export const fetchMovieDetails = createAsyncThunk('movies/fetchMovieDetails', async (movieId) => {
   try {
-    const response = await axios.get(`http://www.omdbapi.com/?i=${movieId}&apikey=25d01e11`);
+    const response = await axios.get(`${OMDB_BASE_URL}?i=${movieId}&apikey=${OMDB_API_KEY}`);
     const { data } = response;
     return data;
   } catch (error) {
     throw new Error('Failed to fetch movie details');
   }
-  
 });
-
-
--- src/api.js
+++
@@ -1,3 +0,0 @@
-// api.js
-export const OMDB_BASE_URL = 'http://www.omdbapi.com/';
-export const OMDB_API_KEY = '25d01e11';
